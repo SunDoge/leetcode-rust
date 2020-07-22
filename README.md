c1eda9b9d1afb7eca7b5a32196043c376324e427
SunDoge <384813529@qq.com>
2020-07-22 13:09:21 +0800

    Add valid parenthese

--- README.md
+++ README.md
@@ -11,6 +11,7 @@
 | 9 | [palindrome number](src/bin/9-palindrome-number.rs) | `cargo run --bin 9-palindrome-number` |
 | 13 | [roman to integer](src/bin/13-roman-to-integer.rs) | `cargo run --bin 13-roman-to-integer` |
 | 14 | [longest common prefix](src/bin/14-longest-common-prefix.rs) | `cargo run --bin 14-longest-common-prefix` |
+| 20 | [valid parentheses](src/bin/20-valid-parentheses.rs) | `cargo run --bin 20-valid-parentheses` |
 | 27 | [remove element](src/bin/27-remove-element.rs) | `cargo run --bin 27-remove-element` |
 | 36 | [valid sudoku](src/bin/36-valid-sudoku.rs) | `cargo run --bin 36-valid-sudoku` |
 | 42 | [trapping rain water](src/bin/42-trapping-rain-water.rs) | `cargo run --bin 42-trapping-rain-water` |
---
+++ src/bin/20-valid-parentheses.rs
@@ -0,0 +1,42 @@
+pub fn is_valid(s: String) -> bool {
+    if s.len() % 2 == 1 {
+        return false;
+    }
+
+    let mut stack = Vec::new();
+
+    for &ch in s.as_bytes() {
+        match ch {
+            b'(' => stack.push(b')'),
+            b'[' => stack.push(b']'),
+            b'{' => stack.push(b'}'),
+            other => {
+                if let Some(last) = stack.pop() {
+                    if other != last {
+                        return false;
+                    }
+                } else {
+                    // 如果上来就是right，直接invalid
+                    return false;
+                }
+            }
+        }
+    }
+
+    // 必须所有括号都闭合
+    if stack.is_empty() {
+        true
+    } else {
+        false
+    }
+}
+
+fn main() {
+    let s = "()".to_string();
+    assert_eq!(is_valid(s), true);
+    let s = "()[]{}".to_string();
+    assert_eq!(is_valid(s), true);
+
+    let s = "([)]".to_string();
+    assert_eq!(is_valid(s), false);
+}
